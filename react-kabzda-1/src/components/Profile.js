3178fd1aed5c627fbbfe9b326aca8e9ad4398549
Daniil Bondarchuk <bondarchuk19988@gmail.com>
2025-10-08 17:16:36 +0700

    Добавлена папка react-kabzda-1

---
+++ react-kabzda-1/src/components/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+          <img src='https://avatars.mds.yandex.net/i?id=0171d281f456486f6627e0aeacc7e0e2067d1008-5250085-images-thumbs&n=13'></img>
+        </div>
+        <div>
+          ava + description
+        </div>
+        <div>
+          My post
+        </div>
+        <div>
+          New post
+        </div>
+        <div>
+          <div>
+            post 1
+          </div>
+          <div>
+            post 2
+          </div>
+        </div>
+      </div>
+}
+
+export default Profile;
